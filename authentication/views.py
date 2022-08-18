db779f48f45cb93806e87bef7bc8eb664085ee76
Darth <redzed427@gmail.com>
2022-08-18 11:09:33 +0100

    Django_course_shop_lesson_4_part_2

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import logout, authenticate, login
+from django.shortcuts import render, redirect
+
+from authentication.forms import LoginForm
+
 
 def login_user(request):
-    return render(request, 'auth/login.html')
+    context = {'login_form': LoginForm()}
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')
+
+    return render(request, 'auth/login.html', context)
+
 
 def register(request):
     return render(request, 'auth/register.html')
 
+
 def logout_user(request):
-    pass
+    logout(request)
+    return redirect('index')
 
