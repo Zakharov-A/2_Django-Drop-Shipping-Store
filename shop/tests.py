593c44393c7e3621d5be8058276a83f1bf86c5e9
Darth <redzed427@gmail.com>
2022-08-24 13:11:59 -0400

    Django_course_shop_lesson_5_part_1

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from shop.models import Product, Payment, OrderItem, Order
+
+
+class TestDataBase(TestCase):
+    fixtures = [
+        "shop/fixtures/data.json"
+    ]
+
+    def setUp(self):
+        self.user = User.objects.get(username='root')
+
+    def test_user_exists(self):
+        users = User.objects.all()
+        users_number = users.count()
+        user = users.first()
+        self.assertEqual(users_number, 1)
+        self.assertEqual(user.username, 'root')
+        self.assertTrue(user.is_superuser)
+
+    def test_user_check_password(self):
+        self.assertTrue(self.user.check_password('123'))
+
+
+
