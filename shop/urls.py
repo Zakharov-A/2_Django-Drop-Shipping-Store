f20ebbbfb80c6e74cbe7ed45ad520721f052e0c2
Darth <redzed427@gmail.com>
2022-09-18 02:11:01 -0400

    Django_course_shop_lesson_7_part_2

--- shop/urls.py
+++ shop/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from shop import views
 
 urlpatterns = [
     path('fill-database/', views.fill_database, name='fill_database'),
+    path('', TemplateView.as_view(template_name='shop/shop.html'), name='shop'),
+    path('cart_view/',
+         TemplateView.as_view(template_name='shop/cart.html'), name='cart_view'),
+    path('detail/<int:pk>/',
+         TemplateView.as_view(template_name='shop/shop-details.html'),
+         name='shop_detail')
 ]
